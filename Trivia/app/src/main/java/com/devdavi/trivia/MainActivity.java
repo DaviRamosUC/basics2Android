ff94265ff5316bbf5a5828e667293c17f969a40c
Davi Ramos <ifbadavi@gmail.com>
2022-01-27 20:50:15 -0300

    fetching data from trivia API with Volley

--- Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
+++ Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
@@ -1,14 +1,29 @@
 package com.devdavi.trivia;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.devdavi.trivia.controller.AppController;
 
 public class MainActivity extends AppCompatActivity {
+    String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    Log.d("TAG", "RESPONSE: "+ response.toString());
+                },
+                error -> {
+                }
+        );
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
     }
 }
