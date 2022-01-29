a715c1bc77124627c124238027153a6aa8e5489c
Davi Ramos <ifbadavi@gmail.com>
2022-01-29 20:08:37 -0300

    Check answer, UI Feedback, Adding shake animation

--- Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
+++ Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
@@ -1,6 +1,8 @@
 package com.devdavi.trivia;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -8,6 +10,7 @@ import androidx.databinding.DataBindingUtil;
 import com.devdavi.trivia.data.Repository;
 import com.devdavi.trivia.databinding.ActivityMainBinding;
 import com.devdavi.trivia.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -34,17 +37,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.buttonTrue.setOnClickListener(view -> {
-
+            checkAnswer(true);
         });
 
         binding.buttonFalse.setOnClickListener(view -> {
-
+            checkAnswer(false);
         });
 
     }
 
+    private void checkAnswer(boolean userChoseCorrect) {
+        boolean answer = questionList.get(currentQuestionIndex).isAnswer();
+        int snackMessageId = 0;
+        if (userChoseCorrect == answer) {
+            binding.buttonNext.callOnClick();
+            snackMessageId = R.string.correct_answer;
+        } else {
+            snackMessageId = R.string.incorrect;
+            shakeAnimation();
+            updateQuestion();
+        }
+        Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT)
+                .show();
+    }
+
     private void updateQuestion() {
+        updateCounter();
         String question = questionList.get(currentQuestionIndex).getQuestion();
         binding.questionTextView.setText(question);
+    }
+
+    private void updateCounter() {
+        binding.textViewOutOf.setText(String.format(getString(R.string.text_formated), currentQuestionIndex, questionList.size()));
+    }
+
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake_animation);
+        binding.cardView.setAnimation(shake);
     }
 }
