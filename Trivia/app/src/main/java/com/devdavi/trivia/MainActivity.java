661205b960165ca57e0b6c4026d42ef044ef0a92
Davi Ramos <ifbadavi@gmail.com>
2022-01-29 19:17:32 -0300

    Setup data binding view and show next question

--- Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
+++ Trivia/app/src/main/java/com/devdavi/trivia/MainActivity.java
@@ -1,26 +1,50 @@
 package com.devdavi.trivia;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.devdavi.trivia.data.Repository;
+import com.devdavi.trivia.databinding.ActivityMainBinding;
 import com.devdavi.trivia.model.Question;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questionList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList -> {
-            Log.d("MAIN", "onCreate: " + questionArrayList);
+        questionList = new Repository().getQuestions(questionArrayList -> {
+            updateQuestion();
         });
 
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+            updateQuestion();
+        });
+
+        binding.buttonTrue.setOnClickListener(view -> {
+
+        });
+
+        binding.buttonFalse.setOnClickListener(view -> {
+
+        });
+
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getQuestion();
+        binding.questionTextView.setText(question);
     }
 }
