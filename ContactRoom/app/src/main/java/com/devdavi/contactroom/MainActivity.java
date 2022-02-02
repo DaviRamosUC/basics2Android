c6ad998defe20fb022914c46a413bc1209c0894d
Davi Ramos <ifbadavi@gmail.com>
2022-02-02 12:45:35 -0300

    Setup RecyclerView adapter

--- ContactRoom/app/src/main/java/com/devdavi/contactroom/MainActivity.java
+++ ContactRoom/app/src/main/java/com/devdavi/contactroom/MainActivity.java
@@ -2,21 +2,29 @@ package com.devdavi.contactroom;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.devdavi.contactroom.adapter.RecyclerViewAdapter;
 import com.devdavi.contactroom.model.Contact;
 import com.devdavi.contactroom.model.ContactViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ContactViewModel contactViewModel;
     private TextView textView;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private LiveData<List<Contact>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +32,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton fab = findViewById(R.id.add_contact_fab);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this
                 .getApplication())
                 .create(ContactViewModel.class);
 
         contactViewModel.getAllContacts().observe(this, contacts -> {
-            StringBuilder builder = new StringBuilder();
-            for (Contact c : contacts) {
-                builder.append(" - ").append(c.getName()).append(" ").append(c.getOccupation());
-                Log.d("Main", "onCreate: " + c.getName());
-            }
+
+            // Set up adapter
+            recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, contacts);
+            recyclerView.setAdapter(recyclerViewAdapter);
         });
 
         fab.setOnClickListener(view -> {
