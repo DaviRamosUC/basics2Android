15d59df67e0cad771b34fae5c7a3a24e17637f23
Davi Ramos <ifbadavi@gmail.com>
2022-02-03 11:01:57 -0300

    Saving Task from BottomSheet dialog

--- Todoister/app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
+++ Todoister/app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
@@ -1,18 +1,38 @@
 package com.bawp.todoister;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.Group;
 
+import com.bawp.todoister.model.Task;
+import com.bawp.todoister.model.TaskViewModel;
+import com.bawp.todoister.model.enums.Priority;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.chip.Chip;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+    private EditText enterTodo;
+    private ImageButton calendarButton;
+    private ImageButton priorityButton;
+    private RadioGroup priorityRadioGroup;
+    private RadioGroup selectedRadioButton;
+    private int selectedButtonId;
+    private ImageButton saveButton;
+    private CalendarView calendarView;
+    private Group calendarGroup;
 
     public BottomSheetFragment() {
     }
@@ -24,12 +44,41 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     ) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        // Adjust the size of bottom_sheet fragment
         Objects.requireNonNull(getDialog()).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        calendarGroup = view.findViewById(R.id.calendar_group);
+        calendarView = view.findViewById(R.id.calendar_view);
+        calendarButton = view.findViewById(R.id.today_calendar_button);
+        enterTodo = view.findViewById(R.id.enter_todo_et);
+        saveButton = view.findViewById(R.id.save_todo_button);
+        priorityButton = view.findViewById(R.id.priority_todo_button);
+        priorityRadioGroup = view.findViewById(R.id.radioGroup_priority);
+
+        Chip todayChip = view.findViewById(R.id.today_chip);
+        Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+
+
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        saveButton.setOnClickListener(view1 -> {
+            String task = enterTodo.getText().toString().trim();
+            if (!TextUtils.isEmpty(task)) {
+                Task myTask = new Task(
+                        task,
+                        Priority.HIGH,
+                        Calendar.getInstance().getTime(),
+                        Calendar.getInstance().getTime(),
+                        false
+                );
+                TaskViewModel.insert(myTask);
+            }
+        });
 
     }
 }
