b2cbea794e509a7b1bd80711be357b9fa1552e61
Davi Ramos <ifbadavi@gmail.com>
2022-02-03 19:09:24 -0300

    Adding priority colors to each row

--- Todoister/app/src/main/java/com/bawp/todoister/adapter/ReciclerViewAdapter.java
+++ Todoister/app/src/main/java/com/bawp/todoister/adapter/ReciclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.bawp.todoister.adapter;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,8 +40,20 @@ public class ReciclerViewAdapter extends RecyclerView.Adapter<ReciclerViewAdapte
         Task task = taskList.get(position);
         String formattedDate = Utils.formatDate(task.getDueDate());
 
+        ColorStateList colorStateList = new ColorStateList(new int[][]{
+                new int[]{-android.R.attr.state_enabled},
+                new int[]{android.R.attr.state_enabled}
+        },
+                new int[]{
+                        Color.LTGRAY, //disabled
+                        Utils.priorityColor(task)
+                });
+
         holder.task.setText(task.getTask());
         holder.todayChip.setText(formattedDate);
+        holder.todayChip.setTextColor(Utils.priorityColor(task));
+        holder.todayChip.setChipIconTint(colorStateList);
+        holder.radioButton.setButtonTintList(colorStateList);
     }
 
     @Override
