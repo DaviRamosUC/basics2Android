a486183dccb471002a709c8ed36ce064166642f8
Davi Ramos <ifbadavi@gmail.com>
2022-02-03 11:56:21 -0300

    Saving Due Date along with the Task

--- Todoister/app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
+++ Todoister/app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
@@ -19,8 +19,10 @@ import com.bawp.todoister.model.TaskViewModel;
 import com.bawp.todoister.model.enums.Priority;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
@@ -33,6 +35,8 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     private ImageButton saveButton;
     private CalendarView calendarView;
     private Group calendarGroup;
+    private Date dueDate;
+    Calendar calendar = Calendar.getInstance();
 
     public BottomSheetFragment() {
     }
@@ -66,17 +70,41 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        calendarButton.setOnClickListener(view1 -> {
+            calendarGroup.setVisibility(
+                    calendarGroup.getVisibility() == View.GONE ? View.VISIBLE : View.GONE
+            );
+        });
+
+        calendarView.setOnDateChangeListener((calendarView, year, month, dayOfMonth) -> {
+            calendar.clear();
+            calendar.set(year, month, dayOfMonth);
+            dueDate = calendar.getTime();
+//            Log.d("CAL", "onViewCreated: ===> month "+ (month + 1)+ ", dayOfMonth "+ dayOfMonth);
+        });
+
         saveButton.setOnClickListener(view1 -> {
             String task = enterTodo.getText().toString().trim();
-            if (!TextUtils.isEmpty(task)) {
+            if (!TextUtils.isEmpty(task) && dueDate != null) {
                 Task myTask = new Task(
                         task,
                         Priority.HIGH,
-                        Calendar.getInstance().getTime(),
+                        dueDate,
                         Calendar.getInstance().getTime(),
                         false
                 );
                 TaskViewModel.insert(myTask);
+            } else {
+                String snackbarText;
+                if (TextUtils.isEmpty(task)) {
+                    snackbarText = "Enter the TODO text";
+                } else {
+                    snackbarText = "Chose the due date for this task";
+                    if (calendarGroup.getVisibility()==View.GONE){
+                        calendarButton.callOnClick();
+                    }
+                }
+                Snackbar.make(Objects.requireNonNull(getDialog()).getWindow().getDecorView(), snackbarText, Snackbar.LENGTH_SHORT).show();
             }
         });
 
