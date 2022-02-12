1e040ecda3ec1ae47ae3f0914e31cbe5794d01f3
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-02-12 12:45:06 -0300

    Learn about listView & adapter

---
+++ ListView/app/src/main/java/com/devdavi/listview/MainActivity.java
@@ -0,0 +1,59 @@
+package com.devdavi.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos Reis", "Caldas Novas",
+            "Campos do Jordão", "Costa do Sauípe",
+            "Ilhéus", "Porto Seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Arauba"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //Criar o adapter para a lista
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adiciona o adaptador para a lista
+        listLocais.setAdapter(adapter);
+
+        //adicionar clique na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String valorSelecionado = listLocais.getItemAtPosition(i).toString();
+                Toast.makeText(
+                        getApplicationContext(),
+                        valorSelecionado,
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+    }
+}
