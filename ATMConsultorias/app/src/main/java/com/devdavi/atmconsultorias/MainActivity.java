f2ea9d1dff3e542d854609aaa86174e98aa32930
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-02-15 12:34:39 -0300

    Adding itent to send email on FAB

--- ATMConsultorias/app/src/main/java/com/devdavi/atmconsultorias/MainActivity.java
+++ ATMConsultorias/app/src/main/java/com/devdavi/atmconsultorias/MainActivity.java
@@ -1,5 +1,7 @@
 package com.devdavi.atmconsultorias;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -29,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-
+        binding.appBarMain.fab.setOnClickListener(view -> {
+            enviarEmail();
+        });
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_principal, R.id.nav_servicos,
                 R.id.nav_clientes, R.id.nav_contato, R.id.nav_sobre
@@ -45,6 +47,24 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void enviarEmail() {
+        String celular = "tel:+5571991360431";
+        String imagem = "https://invexo.com.br/blog/wp-content/uploads/2020/06/5-praias-da-zona-sul-para-ir.jpg";
+        String endereco = "https://www.google.com.br/maps/place/Jardim+do+Morro/@41.1370661,-8.6114491,17z/data=!3m1!4b1!4m5!3m4!1s0xd2464ddb5fa061f:0x2939ca79bfea34e7!8m2!3d41.1370621!4d-8.6092604";
+//        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(celular));
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(imagem));
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(endereco));
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ifbadavi@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "A happy new year");
+        intent.putExtra(Intent.EXTRA_TEXT, "Mensagem automática");
+
+        intent.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(intent, "Compartilhar"));
     }
 
     @Override
